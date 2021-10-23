4d5b16ceb9b0c484272fb7ea9bc7ae4402cc1fcb
JOnathan Daniel <gcagrad19@gmail.com>
2021-10-23 01:06:13 -0400

    Refactor + Added Schools Routre

--- server/routes/schoolRoute.js
+++ server/routes/schoolRoute.js
@@ -3,29 +3,6 @@ const express = require("express");
 const schoolRouter = express.Router();
 const { find } = require("../mongo/functions");
 
-let schools = [
-    {
-        name: "Clemson University",
-        buildings: ["Cribb Hall", "Deschamps Hall", "Watt Innovation Center"],
-    },
-    {
-        name: "University of South Carolina",
-        buildings: ["USC Hall", "Deschamps Hall", "Watt Innovation Center"],
-    },
-    {
-        name: "University of Georgia",
-        buildings: [
-            "Franklin Hall",
-            "Deschamps Hall",
-            "Watt Innovation Center",
-        ],
-    },
-    {
-        name: "Georgia Tech",
-        buildings: ["Klaus Hall", "Deschamps Hall", "Watt Innovation Center"],
-    },
-];
-
 schoolRouter.get("", (req, res) => {
     find("Schools", {}, (err, docs) => {
         res.send(docs.map((school) => school.Name));
@@ -33,15 +10,19 @@ schoolRouter.get("", (req, res) => {
 });
 
 schoolRouter.get("/:schoolName", (req, res) => {
-    let returnedSchool = schools.filter(
-        (school) => school.name === req.params.schoolName
-    );
+    find("Schools", {}, (err, docs) => {
+        let school = docs.find(
+            (school) => school.Name === req.params.schoolName
+        );
 
-    if (returnedSchool[0]) {
-        res.send(returnedSchool[0].buildings);
-    } else {
-        res.status(404).send({ error: "School not found" });
-    }
+        if (school.Buildings) {
+            res.send(school.Buildings);
+        } else {
+            res.status(404).send({
+                response: "No buildings found for this school",
+            });
+        }
+    });
 });
 
 module.exports = schoolRouter;
