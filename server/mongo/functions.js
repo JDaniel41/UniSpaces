970b823091635664fd04906d14735835c8b79a01
Tahj Anderson <tahjanderson@235.222.21.198.tigernet.wifi.dyn.clemson.edu>
2021-10-23 11:17:59 -0400

    Scratch

--- server/mongo/functions.js
+++ server/mongo/functions.js
@@ -1,12 +1,27 @@
 const MongoClient = require("mongodb").MongoClient;
+const mongoose = require('mongoose');
 const {Connection} = require('./Connection')
 
 function insert(client){
-
-}
-
-function find (name, query, cb) {
     Connection.open().then((client) => {
+
+    })
+}
+function find (name, query, cb) {
+    Connection.open().then(async (client) => {
+        
+        var SchoolsSchema = new mongoose.Schema({
+            Name: String,
+            Buildings: Array,
+            Building: Array
+        })
+        
+        const doc = await SchoolsSchema.findOne();
+
+        doc instanceof SchoolsSchema; // true
+        doc instanceof mongoose.Model; // true
+        doc instanceof mongoose.Document; // true
+
         const collection = client.db("WebAppData").collection("Schools");
         collection.find(query).toArray(cb);
     }).catch((error) => {
