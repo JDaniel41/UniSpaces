17b10aa258599d2d68c989cc01906119f7c6517e
JOnathan Daniel <gcagrad19@gmail.com>
2021-10-23 11:16:30 -0400

    Added Skeleton for Hopping

--- server/mongo/functions.js
+++ server/mongo/functions.js
@@ -1,17 +1,27 @@
 const MongoClient = require("mongodb").MongoClient;
-const {Connection} = require('./Connection')
+const { Connection } = require("./Connection");
 
-function insert(client){
+function insert(client) {}
 
+function find(name, query, cb) {
+    Connection.open()
+        .then((client) => {
+            const collection = client.db("WebAppData").collection("Schools");
+            collection.find(query).toArray(cb);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
-function find (name, query, cb) {
-    Connection.open().then((client) => {
-        const collection = client.db("WebAppData").collection("Schools");
-        collection.find(query).toArray(cb);
-    }).catch((error) => {
-        console.log(error)
-    });
+function getResponseData(schoolName, buildingName) {
+    // TODO: Get the prompts for this building
+    let prompts = getPrompts(schoolName, buildingName);
+    // TODO: For each prompt, go in the collection and get the responses
 }
 
-module.exports = {find}
+function getPrompts(schoolName, buildingName) {}
+
+function sendResponseData(schoolName, buildingName) {}
+
+module.exports = { find, getResponseData };
