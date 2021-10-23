2e7694c59021d60bfc0b748ae1b471463e1a0690
JOnathan Daniel <gcagrad19@gmail.com>
2021-10-23 10:17:10 -0400

    Added Dummy Response for Building Stats

--- server/routes/buildingStatsRoute.js
+++ server/routes/buildingStatsRoute.js
@@ -1,21 +1,40 @@
 const express = require("express");
 const buildingStatsRouter = express.Router();
 
-let buildingStats = [
-    {
-        school: "Clemson University",
-        buildings: [
+function getDummyData(schoolName, buildingName) {
+    return {
+        school: schoolName,
+        building: buildingName,
+        responses: [
             {
-                name: "Baker Hall",
-                maskObservations: [(1, "Not a lot")],
+                name: "Are people wearing a mask?",
+                choices: ["Not a lot", "About Half", "Everyone is masking"],
+                lastResponse: "Everyone is masking",
+                allResponse: [
+                    [1, "Everyone is masking"],
+                    [2, "About Half"],
+                    [3, "Not a lot"],
+                    [4, "Everyone is masking"],
+                ],
+            },
+            {
+                name: "What is the noise level?",
+                choices: ["No Noise", "Low", "Medium", "High"],
+                lastResponse: "Medium",
+                allResponse: [
+                    [1, "Medium"],
+                    [2, "Low"],
+                    [3, "No Noise"],
+                    [4, "Medium"],
+                ],
             },
         ],
-    },
-];
+    };
+}
 
 buildingStatsRouter.get("/:schoolName/:buildingName", (req, res) => {
     console.log(req.params.sendAllResponses);
-    res.send({});
+    res.send(getDummyData(req.params.schoolName, req.params.buildingName));
 });
 
 module.exports = buildingStatsRouter;
