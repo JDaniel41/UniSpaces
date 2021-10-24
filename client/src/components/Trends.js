09c56105d959a06d48bc99e53b7fa7c93bd6344e
JOnathan Daniel <gcagrad19@gmail.com>
2021-10-23 20:40:24 -0400

    Fixed Trends

--- client/src/components/Trends.js
+++ client/src/components/Trends.js
@@ -1,6 +1,6 @@
-import React, {useEffect} from "react";
-import {useState} from "react";
-import InputForm from "./InputForm.js"
+import React, { useEffect } from "react";
+import { useState } from "react";
+import InputForm from "./InputForm.js";
 import {
     BarChart,
     Bar,
@@ -12,14 +12,12 @@ import {
     Legend,
 } from "recharts";
 
-
-
 // Makes sure that response data is the trended part of the response.
 function Renderbarchart(props) {
-    
-    console.log(props.response)
+    console.log(props.response);
     return (
-        <BarChart key={props.id}
+        <BarChart
+            key={props.id}
             width={500}
             height={300}
             data={props.response}
@@ -35,13 +33,13 @@ function Renderbarchart(props) {
             <YAxis />
             <Tooltip />
             <Legend />
-            <Bar dataKey="freq" fill="#8884d8" />
+            <Bar dataKey="count" fill="#8884d8" />
         </BarChart>
     );
 }
 
 export default function Trends(props) {
-    const axios = require('axios');
+    const axios = require("axios");
 
     const [schools, setSchools] = useState(["None"]);
     const [selectedSchool, setSelectedSchool] = useState(0);
@@ -51,84 +49,87 @@ export default function Trends(props) {
 
     const [data2, setData2] = useState(null);
 
-    function handleChange1(evt){
+    function handleChange1(evt) {
         console.log("hello");
-        console.log(props.schools[evt.target.value-1]);
-        setSelectedSchool(props.schools[evt.target.value-1]);
+        console.log(props.schools[evt.target.value - 1]);
+        setSelectedSchool(props.schools[evt.target.value - 1]);
         console.log(selectedSchool);
     }
-    function handleChange2(evt){
-        setSelectedBuilding(data[evt.target.value-1]);
+    function handleChange2(evt) {
+        setSelectedBuilding(data[evt.target.value - 1]);
         console.log(selectedBuilding);
     }
 
     useEffect(() => {
-        async function updateSchools(){
+        async function updateSchools() {
             setSchools(props.schools);
         }
         updateSchools();
     }, [props]);
 
-    useEffect (() => {
-        function getBuildings (){
-            axios.get(`https://hackgt-unispaces.herokuapp.com/schools/${selectedSchool}/`)
-            .then(function (response) {
-            if (response.status === 200){
-                console.log(response);
-                setData(response.data);
-                }
-            })
-            .catch(function (error) {
-            console.log(error);
-            })
+    useEffect(() => {
+        function getBuildings() {
+            axios
+                .get(
+                    `https://hackgt-unispaces.herokuapp.com/schools/${selectedSchool}/`
+                )
+                .then(function (response) {
+                    if (response.status === 200) {
+                        console.log(response);
+                        setData(response.data);
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
         }
         getBuildings();
         return () => {
             setData(["none"]);
-        }
-        
-    }, [axios, selectedSchool])
+        };
+    }, [axios, selectedSchool]);
 
-    useEffect (() => {
-        function getTrendData (){
-            axios.get(`https://hackgt-unispaces.herokuapp.com/stats/${selectedSchool}/${selectedBuilding}`)
-            .then(function (response) {
-            if (response.status === 200){
-                console.log(response);
-                setData2(response);
-                }
-            })
-            .catch(function (error) {
-            console.log(error);
-            })
+    useEffect(() => {
+        function getTrendData() {
+            axios
+                .get(
+                    `https://hackgt-unispaces.herokuapp.com/stats/${selectedSchool}/${selectedBuilding}`
+                )
+                .then(function (response) {
+                    if (response.status === 200) {
+                        console.log(response);
+                        setData2(response);
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
         }
         getTrendData();
-        
-    }, [axios, selectedBuilding])
+    }, [axios, selectedBuilding]);
 
     return (
         <div className="Trend">
             <h1>Get The Latest Trends Of A Building On Your Campus</h1>
             <div className="get-margins">
-            <h2>Location Selection</h2>
-            <InputForm options={schools} onSelectChange={handleChange1}/>
-            <br/>
-            
-            {
-            data !== null &&
-            <InputForm options={data} onSelectChange={handleChange2}/>
-            }
-            
-            <hr/>
+                <h2>Location Selection</h2>
+                <InputForm options={schools} onSelectChange={handleChange1} />
+                <br />
 
-            { data2 !== null && 
-            data2.data.responses.map((info, index) =>(
-                <Renderbarchart id={index} response={info.trainedResponses}/>
-            ))}
-            
+                {data !== null && (
+                    <InputForm options={data} onSelectChange={handleChange2} />
+                )}
+
+                <hr />
+
+                {data2 !== null &&
+                    data2.data.responses.map((info, index) => (
+                        <Renderbarchart
+                            id={index}
+                            response={info.trainedResponses}
+                        />
+                    ))}
             </div>
-
-            
         </div>
     );
 }
