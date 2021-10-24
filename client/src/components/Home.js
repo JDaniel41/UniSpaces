bad515d3a5a1449f3bd26fb815d8f23127730b36
Aaron <anwoolf@clemson.edu>
2021-10-23 21:01:32 -0400

    front page images

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "../styles/Home.css";
 import Logo from "../img/logo4.png";
+import image2 from "../img/2.png";
+import image3 from "../img/3.png";
+import image4 from "../img/4.png";
 // import Logo2 from "../img/Logo2.jpg";
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
@@ -8,47 +11,45 @@ import CardGroup from 'react-bootstrap/CardGroup';
 export default function Home() {
     return (
         <div className="Home">
-            <h1>Home</h1>
+            <h1>UniSpaces</h1>
             <img className="Home-logo" src={Logo} alt='Logo'/>
-            <CardGroup>
+            <CardGroup className="Home-margins">
                 <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Img variant="top" src={image2} />
                     <Card.Body>
-                    <Card.Title>Card title</Card.Title>
+                    <Card.Title>Post</Card.Title>
                     <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.
+                        Help update the status of Builidngs on your campus by visiting the Post page.
                     </Card.Text>
                     </Card.Body>
                     <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
+                    <small className="text-muted"></small>
                     </Card.Footer>
                 </Card>
+                <div className="Home-space"></div>
                 <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Img variant="top" src={image3} />
                     <Card.Body>
-                    <Card.Title>Card title</Card.Title>
+                    <Card.Title>Trends</Card.Title>
                     <Card.Text>
-                        This card has supporting text below as a natural lead-in to additional
-                        content.{' '}
+                        Check out the latest trends regarding capacity, mask wearing, and more on buildings on your campus.{' '}
                     </Card.Text>
                     </Card.Body>
                     <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
+                    <small className="text-muted"></small>
                     </Card.Footer>
                 </Card>
+                <div className="Home-space"></div>
                 <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Img variant="top" src={image4} />
                     <Card.Body>
-                    <Card.Title>Card title</Card.Title>
+                    <Card.Title>Get</Card.Title>
                     <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This card has even longer content than the first to
-                        show that equal height action.
+                        Want something more simple? Check out our get page to get the latest status of buildings on your campus
                     </Card.Text>
                     </Card.Body>
                     <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
+                    <small className="text-muted"></small>
                     </Card.Footer>
                 </Card>
             </CardGroup>
